65896a62df2577e96e8d4f95284040621895c0be
NanderSantos <nandermgsdp@gmail.com>
2021-09-28 14:03:54 -0300

    Finzalizaçao da seção 4 do Curso "Spring Boot API REST: Construa uma API": usar a anotation @RequestMapping em cima da classe para definir a rota como geral para todos os métodos HTTP; usar a anotation @PostMapping para mapear requisições do tipo POST; criar uma classe DTO para lidar com os dados enviados no corpo da requisição POST; utilizar o códgio HTTP 201 para dados criados no database; como usar a classe ResponseEntity para montar a resposta da API; utilizar ferramentas de teste de APIs Rest, no caso utilizei o Insomnia.

--- forum/src/main/java/br/com/alura/forum/controller/TopicsController.java
+++ forum/src/main/java/br/com/alura/forum/controller/TopicsController.java
@@ -1,22 +1,34 @@
 package br.com.alura.forum.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.alura.forum.controller.dto.TopicDto;
+import br.com.alura.forum.controller.form.TopicForm;
 import br.com.alura.forum.model.Topic;
+import br.com.alura.forum.repository.CourseRepository;
 import br.com.alura.forum.repository.TopicRepository;
 
-@RestController 
+@RestController
+@RequestMapping("/topics")
 public class TopicsController {
 	
 	@Autowired
 	private TopicRepository topicRepository;
 	
-	@RequestMapping("/topics")
+	@Autowired
+	private CourseRepository courseRepository;
+	
+	@GetMapping
 	public List<TopicDto> list(String courseName) {
 		
 		if(courseName == null) {
@@ -29,5 +41,17 @@ public class TopicsController {
 			List<Topic> topics = topicRepository.findByCourse_Name(courseName);	
 			return TopicDto.convert(topics);
 		}
+	}
+	
+	@PostMapping
+	public ResponseEntity<TopicDto> create(@RequestBody TopicForm form, UriComponentsBuilder uriBuilder) {
+		
+		Topic topic = form.convert(courseRepository);
+		
+		topicRepository.save(topic);
+		
+		URI uri = uriBuilder.path("/topics/{id}").buildAndExpand(topic.getId()).toUri();
+		
+		return ResponseEntity.created(uri).body(new TopicDto(topic));
 	}
 }
--- forum/src/main/java/br/com/alura/forum/model/Topic.java
+++ forum/src/main/java/br/com/alura/forum/model/Topic.java
@@ -38,6 +38,17 @@ public class Topic {
 	@OneToMany(mappedBy = "topic")
 	private List<Response> responses = new ArrayList<>();
 
+	public Topic() {
+	
+	}
+	
+	public Topic(String title, String message, Course course) {
+
+		this.title = title;
+		this.message = message;
+		this.course = course;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
