37ee52ab36ab90a0f936270c1a8a3c2fc8e149b9
NanderSantos <nandermgsdp@gmail.com>
2021-09-30 10:47:47 -0300

    Finalizaçao da seção 5 do Curso "Spring Boot API REST: Segurança da API, Cache e Monitoramento": utilizar o cabeçalho Authorization, passando o valor "Bearer valor-do-token"; criar uma classe que herda a classe OncePerRequestFilter para filtrar as requisições, obtendo o token de autenticação enviado, antes de processar a requisição; dentro do filter utilizar o método request.getHeader("Authorization") para obter o token enviado na requisição; na Classe SecurityConfiguration, no método configure(HttpSecurity http), utilizar o método and().addFilterBefore(new AutenticacaoViaTokenFilter(), UsernamePasswordAuthenticationFilter.class) para habilitar o filtro do token; utilizar a classe SecurityContextHolder, para indicar para o Spring que o cliente está autenticado através do método SecurityContextHolder.getContext().setAuthentication(authentication).

--- forum/src/main/java/br/com/alura/forum/config/security/TokenService.java
+++ forum/src/main/java/br/com/alura/forum/config/security/TokenService.java
@@ -7,6 +7,8 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import br.com.alura.forum.model.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
@@ -15,22 +17,45 @@ public class TokenService {
 
 	@Value("${forum.jwt.expiration}")
 	private String expiration;
-	
+
 	@Value("${forum.jwt.secret}")
 	private String secret;
-	
+
 	public String generateToken(Authentication authentication) {
 
 		User user = (User) authentication.getPrincipal();
 		Date now = new Date();
 		Date expirationDate = new Date(now.getTime() + Long.parseLong(expiration));
-		
-		return Jwts.builder().setIssuer("API do Fórum da Alura")
-				.setSubject(user.getId().toString())
-				.setIssuedAt(now)
-				.setExpiration(expirationDate)
-				.signWith(SignatureAlgorithm.HS256, secret)
-				.compact();
+
+		return Jwts.builder().setIssuer("API do Fórum da Alura").setSubject(user.getId().toString()).setIssuedAt(now)
+				.setExpiration(expirationDate).signWith(SignatureAlgorithm.HS256, secret).compact();
 	}
 
+	public boolean isTokenValid(String token) {
+
+		if (token == null || token.isEmpty())
+			return false;
+		else {
+
+			try {
+
+				Jwts.parser().setSigningKey(this.secret).parseClaimsJws(token);
+
+				return true;
+
+			} catch (ExpiredJwtException e) {
+
+				return false;
+			}
+		}
+	}
+
+	public Long getUserId(String token) {
+
+		Claims claims = Jwts.parser().setSigningKey(this.secret).parseClaimsJws(token).getBody();
+		
+		System.out.println(claims.getSubject());
+		
+		return Long.parseLong(claims.getSubject());
+	}
 }
