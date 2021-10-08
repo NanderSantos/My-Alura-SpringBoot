d4675362ea44eaceffaf5d8f5e678635b88b5eb6
NanderSantos <nandermgsdp@gmail.com>
2021-10-08 11:53:12 -0300

    Finalização da seção 4 do curso "Java e JPA: Como realizar consultas JPQL com parâmetros opcionais; Como funciona a Criteria API da JPA; Como realizar uma consulta com parâmetros opcionais via Criteria API.

--- store/src/main/java/br/com/alura/store/dao/ProductDao.java
+++ store/src/main/java/br/com/alura/store/dao/ProductDao.java
@@ -3,7 +3,13 @@ package br.com.alura.store.dao;
 import br.com.alura.store.model.Product;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 
 public class ProductDao {
@@ -68,5 +74,54 @@ public class ProductDao {
                 .createQuery(jpql, BigDecimal.class)
                 .setParameter("name", name)
                 .getSingleResult();
+    }
+
+    public List<Product> findByOldOptionalParameters(String name, BigDecimal price, LocalDate creationDate) {
+
+        String jpql = "SELECT p FROM Product p WHERE 1=1";
+
+        boolean isNamePresent = name != null && !name.trim().isEmpty();
+        boolean isPricePresent = price != null;
+        boolean isCreationDatePresent = creationDate != null;
+
+        jpql += isNamePresent ? " AND p.name = :name" : "";
+        jpql += isPricePresent ? " AND p.price = :price" : "";
+        jpql += isCreationDatePresent ? " AND p.creationDate = :creationDate" : "";
+
+        TypedQuery<Product> query = this.entityManager.createQuery(jpql, Product.class);
+
+        if (isNamePresent) query.setParameter("name", name);
+        if (isPricePresent) query.setParameter("price", price);
+        if (isCreationDatePresent) query.setParameter("creationDate", creationDate);
+
+        return query.getResultList();
+    }
+
+    public List<Product> findByCriteriaOptionalParameters(String name, BigDecimal price, LocalDate creationDate) {
+
+        boolean isNamePresent = name != null && !name.trim().isEmpty();
+        boolean isPricePresent = price != null;
+        boolean isCreationDatePresent = creationDate != null;
+
+        CriteriaBuilder criteriaBuilder = this.entityManager.getCriteriaBuilder();
+
+        CriteriaQuery<Product> query = criteriaBuilder.createQuery(Product.class);
+        Root<Product> from = query.from(Product.class);
+        Predicate filters = criteriaBuilder.and();
+
+        if (isNamePresent)
+            filters = criteriaBuilder.and(filters, criteriaBuilder.equal(from.get("name"), name));
+
+
+        if (isPricePresent)
+            filters = criteriaBuilder.and(filters, criteriaBuilder.equal(from.get("price"), price));
+
+
+        if (isCreationDatePresent)
+            filters = criteriaBuilder.and(filters, criteriaBuilder.equal(from.get("creationDate"), creationDate));
+
+        query.where(filters);
+
+        return this.entityManager.createQuery(query).getResultList();
     }
 }
