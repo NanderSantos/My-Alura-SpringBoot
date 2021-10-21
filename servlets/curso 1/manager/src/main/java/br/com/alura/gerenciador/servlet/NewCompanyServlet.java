5d55ba122b8cc5b3f35060dc822730b59fcd9ae8
NanderSantos <nandermgsdp@gmail.com>
2021-10-21 14:29:04 -0300

    Finzalizaçao da seção 1 do Curso "Java Servlet: Fundamentos da programação web Java": escrever uma servlet que atende apenas GET ou POST. ler parâmetros da requisição dentro da servlet; criar um formulário HTML usando as tags form e input; enviar os dados pelo formulário através do POST; diferença entre GET e POST.

--- servlets/curso 1/manager/src/main/java/br/com/alura/gerenciador/servlet/NewCompanyServlet.java
+++ servlets/curso 1/manager/src/main/java/br/com/alura/gerenciador/servlet/NewCompanyServlet.java
@@ -3,26 +3,29 @@ package br.com.alura.gerenciador.servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = "/hello")
-public class HellorWorldServlet extends HttpServlet {
-	
+@WebServlet("/new-company")
+public class NewCompanyServlet extends HttpServlet {
+
 	private static final long serialVersionUID = 1L;
-	
+
 	@Override
-	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		System.out.println("Cadastrando nova empresa");
 
 		PrintWriter out = resp.getWriter();
+		String name = req.getParameter("name");
+		
 		out.println("<html>");
 		out.println("<body>");
-		out.println("Hello, World from servlet!");
+		out.println("Empresa " + name + " cadastrada com sucesso!");
 		out.println("</body>");
 		out.println("</html>");
-		
-		System.out.println("HellorWorldServlet chamado");
 	}
 }
