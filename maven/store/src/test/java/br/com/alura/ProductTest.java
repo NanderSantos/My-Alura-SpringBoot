1f83fa3736e36d88acb285de7ce6bccf62da0064
NanderSantos <nandermgsdp@gmail.com>
2021-10-27 14:10:14 -0300

    Finzalizaçao da seção 2 do Curso "Maven: Gerenciamento de dependências e build de aplicações Java": criar uma aplicação com Maven; entender a estrutura de diretórios de uma aplicação com Maven; migrar uma aplicação para o Maven.

---
+++ maven/store/src/test/java/br/com/alura/ProductTest.java
@@ -0,0 +1,14 @@
+package br.com.alura;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ProductTest {
+	
+	@Test
+	public void teste() {
+
+		assertEquals("", "");
+	}
+}
