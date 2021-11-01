dbd5896aa4f9fa447eee922b5cf3ec51c9f059c1
NanderSantos <nandermgsdp@gmail.com>
2021-11-01 15:15:48 -0300

    Finzalizaçao da seção 1 do Curso "Kotlin: primeiros passos e Orientação a Objetos": O que é o Kotlin; Como acessar a documentação; Criar um projeto Kotlin com IntelliJ; Executar um código em kotlin; Criar variáveis e imprimí-las.

---
+++ bytebank/src/main/kotlin/main.kt
@@ -0,0 +1,13 @@
+fun main() {
+
+    println("\nBem vindo ao ByteBank!\n")
+
+    val titular = "Alex"
+    val numeroConta = 1000
+    var saldo = 0.0
+    saldo += 100
+
+    println("titular: $titular")
+    println("numeroConta: $numeroConta")
+    println("saldo: $saldo")
+}
