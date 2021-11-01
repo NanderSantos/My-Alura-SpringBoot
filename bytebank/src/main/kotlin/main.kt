2b463d56e5053f9d784613c4e82a84117901d6a3
NanderSantos <nandermgsdp@gmail.com>
2021-11-01 15:46:42 -0300

    Finzalizaçao da seção 2 do Curso "Kotlin: primeiros passos e Orientação a Objetos": Estruturas condicionais; Estruturas de repetição; Diferença entre programação procedural e orientada a objetos.

--- bytebank/src/main/kotlin/main.kt
+++ bytebank/src/main/kotlin/main.kt
@@ -2,12 +2,36 @@ fun main() {
 
     println("\nBem vindo ao ByteBank!\n")
 
-    val titular = "Alex"
-    val numeroConta = 1000
-    var saldo = 0.0
-    saldo += 100
+    val holder = "Alex"
+    val accountNumber = 1000
+    var accountBalance = 0.0
+    accountBalance += 100
 
-    println("titular: $titular")
-    println("numeroConta: $numeroConta")
-    println("saldo: $saldo")
+    println("titular: $holder")
+    println("numeroConta: $accountNumber")
+    println("saldo: $accountBalance")
+
+    testConditions(accountBalance)
+    loops()
+}
+
+fun testConditions(accountBalance: Double) {
+
+    println("\ntestConditions:")
+
+    when {
+
+        accountBalance > 0.0 -> println("Conta com saldo positivo")
+        accountBalance == 0.0 -> println("Conta com saldo negativo")
+        else -> println("Conta com saldo nulo")
+    }
+}
+
+fun loops() {
+
+    println("\nloops:")
+
+    for (i in 1..5 step 2) println("Contando de forma crescente: $i")
+    for (i in 1 until 5 step 1) println("Contando de forma crescente: $i")
+    for (i in 5 downTo 1 step 2) println("Contando de forma decrescente: $i")
 }
