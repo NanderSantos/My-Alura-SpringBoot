b6ddc2cf375ebff8be9cabeab8b485bbdf2c2d79
Nander Santos do Carmo <nander.carmo@HOTBHZA003177.local>
2021-12-03 11:38:31 -0300

    Finalização da seção 3 do curso "Testes de Integração: Testes de SQL e DAOs automatizados em Java": escrever outros cenários de testes; como utilizar os recursos do JUnit para organizar o código de testes; importância de manter o código de testes bem organizado e fácil de manter.

--- tests/auction/src/test/java/br/com/alura/auction/dao/UsuarioDaoTest.java
+++ tests/auction/src/test/java/br/com/alura/auction/dao/UsuarioDaoTest.java
@@ -2,29 +2,55 @@ package br.com.alura.auction.dao;
 
 import br.com.alura.auction.model.Usuario;
 import br.com.alura.auction.util.JPAUtil;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UsuarioDaoTest {
 
     private UsuarioDao dao;
+    private EntityManager em;
+
+    @BeforeEach
+    public void beforeEach() {
+        this.em = JPAUtil.getEntityManager();
+        this.dao = new UsuarioDao(em);
+        this.em.getTransaction().begin();
+    }
+
+    @AfterEach
+    public void afterEach() {
+        this.em.getTransaction().rollback();
+    }
+
+    private Usuario criarUsuario() {
+
+        Usuario usuario =  new Usuario("fulano", "fulano@email.com", "12345678");
+        this.em.persist(usuario);
+
+        return usuario;
+    }
 
     @Test
-    void buscarPorUsername() {
+    void deveriaEncontrarUsuarioCadastrado() {
 
-        EntityManager em = JPAUtil.getEntityManager();
-        this.dao = new UsuarioDao(em);
-
-        Usuario usuario = new Usuario("fulano", "fulano@email.com", "12345678");
-        em.getTransaction().begin();
-        em.persist(usuario);
-        em.getTransaction().commit();
-
+        Usuario usuario = criarUsuario();
         Usuario usuarioEncontrado = this.dao.buscarPorUsername(usuario.getNome());
+
         assertNotNull(usuarioEncontrado);
+    }
+
+    @Test
+    void naoDeveriaEncontrarUsuarioNaoCadastrado() {
+
+        criarUsuario();
+
+        assertThrows(NoResultException.class, () -> this.dao.buscarPorUsername("fulano1"));
     }
 
     @Test
