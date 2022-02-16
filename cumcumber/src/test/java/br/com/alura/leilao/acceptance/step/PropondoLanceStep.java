b9b15e7d54f453d6c210e563d5aa0fba5fc76f04
Nander Santos do Carmo <nander.carmo@ip-192-168-124-174.ec2.internal>
2022-02-16 14:41:21 -0300

    Finalização da seção 2 do curso "BDD e Java: Behavior Driven Development com Cucumber": um arquivo .feature pode ter vários cenários e passos (steps); os métodos associado aos passos são reaproveitados entre cenários; podemos passar parâmetros do cenário ao método; Cucumber possui anotações para inicializar (@Before) e finalizar (@After) o cenários; os métodos anotados com @Before e @After são chamados de Hooks; cuidado, pois os Hooks não são visíveis no arquivo .feature.

--- cumcumber/src/test/java/br/com/alura/leilao/acceptance/step/PropondoLanceStep.java
+++ cumcumber/src/test/java/br/com/alura/leilao/acceptance/step/PropondoLanceStep.java
@@ -3,35 +3,77 @@ package br.com.alura.leilao.acceptance.step;
 import br.com.alura.leilao.model.Lance;
 import br.com.alura.leilao.model.Leilao;
 import br.com.alura.leilao.model.Usuario;
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
+import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
+import io.cucumber.java.Before;
+import io.cucumber.java.BeforeStep;
 import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.Então;
 import io.cucumber.java.pt.Quando;
 import org.junit.Assert;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PropondoLanceStep {
 
     private Lance lance;
+    private List<Lance> lances = new ArrayList<>();
     private Leilao leilao;
 
-    @Dado("um lance valido")
+    @Before
+    public void setup() {
+        this.lances = new ArrayList<>();
+        this.leilao = new Leilao("Tablet XPTO");
+        System.out.println("Running scenario... ");
+    }
+
+    @After
+    public void tearDown() {
+        System.out.println("Scenario done!\n");
+    }
+
+    @BeforeStep
+    public void runningStep() {
+        System.out.print("Running step... ");
+    }
+
+    @AfterStep
+    public void finishedStep() {
+        System.out.println("Done!");
+    }
+
+    @Dado("um lance válido")
     public void um_lance_valido() {
         this.lance = new Lance(new Usuario("fulano"), BigDecimal.TEN);
     }
 
-    @Quando("propoe o lance")
-    public void propoe_o_lance() {
-        this.leilao = new Leilao("Tablet XPTO");
+    @Quando("propõe ao leilão")
+    public void propoe_ao_leilao() {
         this.leilao.propoe(this.lance);
     }
 
-    @Então("o lance eh aceito")
+    @Então("o lance é aceito")
     public void o_lance_eh_aceito() {
         Assert.assertEquals(1, this.leilao.getLances().size());
-        Assert.assertEquals(BigDecimal.TEN, leilao.getLances().get(0).getValor());
+        Assert.assertEquals(BigDecimal.TEN, this.leilao.getLances().get(0).getValor());
+    }
+
+    @Dado("um lance de {double} reais do usuário {string}")
+    public void um_lance_de_reais_do_usuario_fulano(Double valor, String nomeUsuario) {
+        this.lances.add(new Lance(new Usuario(nomeUsuario), new BigDecimal(valor)));
+    }
+
+    @Quando("propõe vários lances ao leilão")
+    public void propoe_varios_lances_ao_leilao() {
+        this.lances.forEach(lance -> this.leilao.propoe(lance));
+    }
+
+    @Então("os lances são aceitos")
+    public void os_lances_sao_aceitos() {
+        Assert.assertEquals(2, this.leilao.getLances().size());
+        Assert.assertEquals(this.lances.get(0).getValor(), this.leilao.getLances().get(0).getValor());
+        Assert.assertEquals(this.lances.get(1).getValor(), this.leilao.getLances().get(1).getValor());
     }
 }
--- cumcumber/src/test/resources/features/propondo_lances.feature
+++ cumcumber/src/test/resources/features/propondo_lances.feature
@@ -1,7 +1,14 @@
 # language: pt
 
 Funcionalidade: Propondo lances
+
   Cenário: Propondo um único lance válido
-    Dado um lance valido
-    Quando propoe o lance
-    Então o lance eh aceito
+    Dado um lance válido
+    Quando propõe ao leilão
+    Então o lance é aceito
+
+  Cenário: Propondo vários lances válidos
+    Dado um lance de 10.00 reais do usuário "fulano"
+    E um lance de 15.00 reais do usuário "beltrano"
+    Quando propõe vários lances ao leilão
+    Então os lances são aceitos
