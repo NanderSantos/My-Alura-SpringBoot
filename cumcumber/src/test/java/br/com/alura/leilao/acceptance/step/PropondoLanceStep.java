cc1743a6324296228267d62e8c02a407b3754a8d
Nander Santos do Carmo <nander.carmo@ip-192-168-124-111.ec2.internal>
2022-02-21 15:52:41 -0300

    Finalização da seção 4 do curso "BDD e Java: Behavior Driven Development com Cucumber": usar Exemplos para alimentar o mesmo teste com dados diferentes; usar DataTables para passar vários dados ao teste de uma vez só.

--- cumcumber/src/test/java/br/com/alura/leilao/acceptance/step/PropondoLanceStep.java
+++ cumcumber/src/test/java/br/com/alura/leilao/acceptance/step/PropondoLanceStep.java
@@ -3,6 +3,7 @@ package br.com.alura.leilao.acceptance.step;
 import br.com.alura.leilao.model.Lance;
 import br.com.alura.leilao.model.Leilao;
 import br.com.alura.leilao.model.Usuario;
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.After;
 import io.cucumber.java.AfterStep;
 import io.cucumber.java.Before;
@@ -15,6 +16,7 @@ import org.junit.Assert;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class PropondoLanceStep {
 
@@ -45,35 +47,64 @@ public class PropondoLanceStep {
     }
 
     @Dado("um lance válido")
-    public void um_lance_valido() {
+    public void umLanceValido() {
         this.lance = new Lance(new Usuario("fulano"), BigDecimal.TEN);
     }
 
     @Quando("propõe ao leilão")
-    public void propoe_ao_leilao() {
+    public void propoeAoLeilao() {
         this.leilao.propoe(this.lance);
     }
 
     @Então("o lance é aceito")
-    public void o_lance_eh_aceito() {
+    public void oLanceEAceito() {
         Assert.assertEquals(1, this.leilao.getLances().size());
         Assert.assertEquals(BigDecimal.TEN, this.leilao.getLances().get(0).getValor());
     }
 
     @Dado("um lance de {double} reais do usuário {string}")
-    public void um_lance_de_reais_do_usuario_fulano(Double valor, String nomeUsuario) {
+    public void umLanceDeReaisDoUsuario(Double valor, String nomeUsuario) {
         this.lances.add(new Lance(new Usuario(nomeUsuario), new BigDecimal(valor)));
     }
 
     @Quando("propõe vários lances ao leilão")
-    public void propoe_varios_lances_ao_leilao() {
+    public void propoeVariosLancesAoLeilao() {
         this.lances.forEach(lance -> this.leilao.propoe(lance));
     }
 
     @Então("os lances são aceitos")
-    public void os_lances_sao_aceitos() {
+    public void osLancesSaoAceitos() {
         Assert.assertEquals(2, this.leilao.getLances().size());
         Assert.assertEquals(this.lances.get(0).getValor(), this.leilao.getLances().get(0).getValor());
         Assert.assertEquals(this.lances.get(1).getValor(), this.leilao.getLances().get(1).getValor());
+    }
+
+    @Dado("um lance inválido de {double} reais e do usuario {string}")
+    public void umLanceInvalidoDeValorReaisEDoUsuarioNomeUsuario(Double valor, String nomeUsuario) {
+        System.out.print(nomeUsuario + " ");
+        this.lance = new Lance(new BigDecimal(valor));
+    }
+
+    @Então("o lance não é aceito")
+    public void oLanceNaoEAceito() {
+        Assert.assertEquals(0, this.leilao.getLances().size());
+    }
+
+    @Dado("dois lances")
+    public void doisLances(DataTable dataTable) {
+
+        List<Map<String, String>> valores = dataTable.asMaps();
+        valores.forEach(mapa -> {
+            this.lances.add(new Lance(
+                    new Usuario(mapa.get("nomeUsuario")),
+                    new BigDecimal(mapa.get("valor"))
+            ));
+        });
+    }
+
+    @Então("o segundo lance não é aceito")
+    public void oSegundoLanceNaoEAceito() {
+        Assert.assertEquals(1, this.leilao.getLances().size());
+        Assert.assertEquals(this.lances.get(0).getValor(), this.leilao.getLances().get(0).getValor());
     }
 }
